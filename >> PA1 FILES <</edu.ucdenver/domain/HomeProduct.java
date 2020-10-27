1c4516ec0c79b2a0c54be4470e645bd978c39019
Samuel Hilfer <63205868+GrowingPaigns@users.noreply.github.com>
2020-10-27 09:30:37 -0600

    Add files via upload

---
+++ >> PA1 FILES <</edu.ucdenver/domain/HomeProduct.java
@@ -0,0 +1,13 @@
+package edu.ucdenver.domain;
+
+public class HomeProduct extends Product{
+    private String location;
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String loc) {
+        this.location = loc;
+    }
+}
