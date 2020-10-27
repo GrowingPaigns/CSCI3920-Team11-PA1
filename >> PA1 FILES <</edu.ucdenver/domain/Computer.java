3d12d7487916e9da449ca896b583926e3408bd0c
Samuel Hilfer <63205868+GrowingPaigns@users.noreply.github.com>
2020-10-27 09:29:50 -0600

    Add files via upload

---
+++ >> PA1 FILES <</edu.ucdenver/domain/Computer.java
@@ -0,0 +1,16 @@
+package edu.ucdenver.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Computer {
+    private ArrayList<String> specs;
+
+    public ArrayList<String> getSpecs() {
+        return specs;
+    }
+
+    public void setSpecs(ArrayList<String> specs) {
+        this.specs = specs;
+    }
+}
