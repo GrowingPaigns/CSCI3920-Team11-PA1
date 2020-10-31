59dd0cd2f87e79b881a38715204a4ffff7bbd48a
Samuel Hilfer <63205868+GrowingPaigns@users.noreply.github.com>
2020-10-31 16:31:19 -0600

    Add files via upload

---
+++ out/production/CSCI3920-Team11-PA1/edu/ucdenver/Sam-AdminApp/Main.java
@@ -0,0 +1,33 @@
+package edu.ucdenver.addminapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    Stage window = null;
+
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("IP-Port.fxml"));
+        primaryStage.setTitle("");
+        primaryStage.setScene(new Scene(root, 720, 544));
+        primaryStage.show();
+        window = primaryStage;
+    }
+
+
+
+
+}
